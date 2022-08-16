036836cf4784e0bc3ed4f3cbc93c76a2b00fae9b
deepakthakur-92 <dt9971678995@gmail.com>
2022-08-16 21:10:06 +0530

    updated DataTransformation file

--- Data_Transformation_Training/DataTransformation.py
+++ Data_Transformation_Training/DataTransformation.py
@@ -47,6 +47,40 @@ class DataTransform:
         log_file.close()
 
 
+    def addQuotesToStringValuesInColumn(self):
+        """
+                Method Name: addQuotesToStringValuesInColumn
+                Description: This method converts all the columns with string datatype such that each value for the column
+                             is enclosed in quotes. This is done to avoid the error while inserting string values in table
+                             as varchar.
+                Output: None
+                On Failure: Exception
+
+                Written By: Deepak Thakur
+                Version: 1.0
+
+        """
+
+        log_file = open("Training_Logs/dataTransformLog.txt", 'a+')
+        try:
+            columns = ['MouseID','Genotype','Treatment','Behavior','class']
+
+            onlyfiles = [f for f in listdir(self.goodDataPath)]
+            for file in onlyfiles:
+                data = pd.read_csv(self.goodDataPath+'/'+file)
+                # list of columns with string datatype variables
+
+                for col in data.columns:
+                    if col in columns: # add quotes in string value
+                        data[col] = data[col].apply(lambda x: "'"+str(x)+"'" if not pd.isna(x) else x)
+
+                data.to_csv(self.goodDataPath+"/"+file, index=None, header=True)
+                self.logger.log(log_file,'%s: Quotes added successfully!!' % file)
+        except Exception as e:
+            self.logger.log(log_file,'Data Transformation failed because:: %s'% e)
+            raise e
+
+
 
 
 
